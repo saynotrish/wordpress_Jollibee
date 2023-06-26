bc8c9a5dbdef6298a271931bed9265f616d63d2c
saynotrish <trishaamv.sayno@gmail.com>
2023-06-26 15:53:19 +0800

    hehe

---
+++ includes/inc-supports.php
@@ -0,0 +1,21 @@
+<?php
+
+function jollibee_support(){
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    add_theme_support('title');
+    add_theme_support('custom-logo');
+
+    
+    add_image_size('banner-sm', 220, 150, true);
+    
+    register_nav_menus(
+        array(
+            'header_menu' => __('Header Menu', 'text_domain'),
+            'footer_menu' => __('Footer Menu', 'text_domain'),
+        ),
+    );
+
+}
+
+add_action('after_setup_theme', 'jollibee_support');
